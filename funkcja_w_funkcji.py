37a14a7bf22759f62c59dda20c29ba38dd7c4731
albim72 <marcin.albiniak72@gmail.com>
2023-10-28 10:30:13 +0200

    Update funkcja_w_funkcji.py

--- funkcja_w_funkcji.py
+++ funkcja_w_funkcji.py
@@ -8,3 +8,29 @@ def oblicz(a,b,c):
 print(oblicz(3,5,3))
 print(n)
 
+#przykład 2
+
+def witaj(imie):
+    return f'dziękujemy za założenie konta: {imie}'
+
+def egzamin(imie,punkty,zaliczono):
+    return f'osoba egzaminowana: {imie}, liczba punktów: {punkty}, egzamin zdany? {zaliczono}'
+
+def fx(a,b):
+    return a*100/b
+
+def osoba(funkcja,*args):
+    return funkcja(*args)
+
+print(osoba(witaj,"Leon"))
+print(osoba(egzamin,"Olga",67,"tak"))
+print(osoba(fx,6,7))
+
+liczby = [3,6,78,29,11,12,-15,0,-34,303,19,18,60,101,0,2,-4,221]
+
+parzysta = list(filter(lambda x:x%2==0,liczby))
+print(parzysta)
+
+cube = list(map(lambda x:x**3,liczby))
+print(cube)
+
