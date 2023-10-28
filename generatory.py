b7468493dcf8615a1f536e6cfba7ce26f1ea67fd
albim72 <marcin.albiniak72@gmail.com>
2023-10-28 11:55:47 +0200

    Update generatory.py

--- generatory.py
+++ generatory.py
@@ -7,4 +7,35 @@ print(liczby())
 
 for p in liczby():
     print(p)
-    
+
+#przykład 2
+def wznowienia(n,k):
+    print("wstrzymanie działania...")
+    yield 1005
+    print("wznowienie działania...")
+
+    n=4*n
+    print("wstrzymanie działania...")
+    yield n+k
+    print("wznowienie działania...")
+
+    print("wstrzymanie działania...")
+    yield n-k
+    print("wznowienie działania...")
+
+    print("wstrzymanie działania...")
+    yield n*k
+    print("wznowienie działania...")
+
+    print("wstrzymanie działania...")
+    yield n/k
+    print("wznowienie działania...")
+
+    print("wstrzymanie działania...")
+    yield n**k
+    print("wznowienie działania...")
+
+for i in wznowienia(12,8):
+    print("*"*40)
+    print(type(i))
+    print(f"zwrócono wartość: {i}")
