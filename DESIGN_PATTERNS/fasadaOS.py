ccff334323669ce1f4032c0fda3f66e83db13b15
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 16:34:21 +0100

    Update fasadaOS.py

--- DESIGN_PATTERNS/fasadaOS.py
+++ DESIGN_PATTERNS/fasadaOS.py
@@ -41,8 +41,43 @@ class FileServer(Server):
     def kill(self, restart=True):
         print(f'Ususwanie {self}')
         self.state = State.restart if restart else State.zombie
-        
+
     def create_file(self,user,name,permission):
         print(f'próba utworzenia pliku: {name}, dla użytkownika: {user}, '
               f'z uprawnieniami: {permission}')
+
+class ProcessServer(Server):
+    def __init__(self):
+        self.name = "ProcessServer"
+        self.state = State.new
+
+    def boot(self):
+        print(f'Bootowanie systemu {self}')
+        self.state = State.running
+
+    def kill(self, restart=True):
+        print(f'Ususwanie {self}')
+        self.state = State.restart if restart else State.zombie
+
+    def create_process(self,user,name):
+        print(f'próba utworzenia procesu: {name}, dla użytkownika: {user}.')
+
+class WindowsServer:
+    pass
+
+class NetworkServer:
+    pass
+
+class OperatingSystem:
+    def __init__(self):
+        self.fs = FileServer()
+        self.ps = ProcessServer()
         
+    def start(self):
+        [i.boot() for i in (self.fs, self.ps)]
+        
+    def create_file(self,user,name,permission):
+        return self.fs.create_file(user,name,permission)
+    
+    def create_process(self,user,name):
+        return self.ps.create_process(user,name)
