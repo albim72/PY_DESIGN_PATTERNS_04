992b2e2c884cea3ec3339a2e246f31604da90e5d
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 09:57:22 +0100

    Update calcMeta.py

--- calcMeta.py
+++ calcMeta.py
@@ -6,3 +6,36 @@ def debug(func):
         print(f"pełna nazwa metody: {func.__qualname__}")
         return func(*args,**kwargs)
     return wrapper
+
+
+def debugmethods(cls):
+    for key,val in vars(cls).items():
+        if callable(val):
+            setattr(cls,key,debug(val))
+    return cls
+
+class debugMeta(type):
+    def __new__(cls, clsname,bases,attrs):
+        obj = super().__new__(cls,clsname,bases,attrs)
+        obj = debugmethods(obj)
+        return obj
+
+class Base(metaclass=debugMeta):
+    pass
+
+class Calc(Base):
+    def add(self,x,y):
+        return 2*x+y
+
+    def multi(self,x,y):
+        return 10*x*y
+
+
+
+mc = Calc()
+
+print(mc.add(4,5))
+print(mc.multi(4,5))
+
+
+print(mc.multi.__name__)
