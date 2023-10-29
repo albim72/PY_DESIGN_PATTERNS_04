aba747c9fa728e33bac59e14b49b076bcabfaf4b
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 16:02:06 +0100

    Update interpreterPattern.py

--- DESIGN_PATTERNS/interpreterPattern.py
+++ DESIGN_PATTERNS/interpreterPattern.py
@@ -108,20 +108,20 @@ def main():
     fridge = Fridge()
 
 
-    test = ('open -> gate',
+    tests = ('open -> gate',
             'close -> garage',
             'turn on -> air condition',
             'turn off -> heating',
             'increase -> boiler temperature -> 5 degrees',
             'decrease -> fridge temperature -> 2 degrees')
-    
+
     open_actions = {
         'gate':gate.open,
         'garage':garage.open,
         'air condition':airco.turn_on,
         'heating':heating.turn_on,
         'boiler temperature':boiler.increase_temperature,
-        'fridge temperature':fridge.increase_temperature   
+        'fridge temperature':fridge.increase_temperature
     }
 
     close_actions = {
@@ -132,3 +132,31 @@ def main():
         'boiler temperature': boiler.decrease_temperature,
         'fridge temperature': fridge.decrease_temperature
     }
+
+    for t in tests:
+        if len(event.parseString(t)) == 2:
+            cmd,dev = event.parseString(t)
+            cmd_str,dev_str = ' '.join(cmd), ' '.join(dev)
+            if 'open' in cmd_str or 'turn on' in cmd_str:
+                open_actions[dev_str]()
+            elif 'close' in cmd_str or 'turn off' in cmd_str:
+                close_actions[dev_str]()
+        elif len(event.parseString(t)) == 3:
+            cmd,dev,arg = event.parseString(t)
+            cmd_str = ' '.join(cmd)
+            dev_str = ' '.join(dev)
+            arg_str = ' '.join(arg)
+            num_arg = 0
+            try:
+                num_arg = int(arg_str.split()[0])
+            except ValueError as err:
+                print(f'expected number but got: {arg_str[0]}')
+
+            if 'increase' in cmd_str and num_arg > 0:
+                open_actions[dev_str](num_arg)
+
+            if 'decrease' in cmd_str and num_arg > 0:
+                close_actions[dev_str](num_arg)
+
+if __name__ == '__main__':
+    main()
