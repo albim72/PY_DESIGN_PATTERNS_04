3094a87096f8b1d9cb85aa77680ffb2296ea24c2
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 15:36:39 +0100

    Update interpreterPattern.py

--- DESIGN_PATTERNS/interpreterPattern.py
+++ DESIGN_PATTERNS/interpreterPattern.py
@@ -3,14 +3,14 @@ from pyparsing import Word,OneOrMore,Optional,Group,Suppress,alphanums
 class Gate:
     def __init__(self):
         self.is_open = False
-        
+
     def __str__(self):
         return 'open' if self.is_open else 'closed'
-    
+
     def open(self):
         print('opening the gate')
         self.is_open = True
-        
+
     def close(self):
         print('closing the gate')
         self.is_open = False
@@ -30,3 +30,33 @@ class Garage:
     def close(self):
         print('closing the garage')
         self.is_open = False
+
+class Aircondition:
+    def __init__(self):
+        self.is_on = False
+
+    def __str__(self):
+        return 'on' if self.is_on else 'off'
+
+    def turn_on(self):
+        print('turning on the air condition')
+        self.is_on = True
+
+    def turn_off(self):
+        print('turning off the air condition')
+        self.is_on = False
+
+class Heating:
+    def __init__(self):
+        self.is_on = False
+
+    def __str__(self):
+        return 'on' if self.is_on else 'off'
+
+    def turn_on(self):
+        print('turning on the heating')
+        self.is_on = True
+
+    def turn_off(self):
+        print('turning off the heating')
+        self.is_on = False
