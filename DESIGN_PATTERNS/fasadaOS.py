1d454dae26c410a94a82ed7c9ac4e76eefbc196d
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 16:37:01 +0100

    Update fasadaOS.py

--- DESIGN_PATTERNS/fasadaOS.py
+++ DESIGN_PATTERNS/fasadaOS.py
@@ -72,12 +72,21 @@ class OperatingSystem:
     def __init__(self):
         self.fs = FileServer()
         self.ps = ProcessServer()
-        
+
     def start(self):
         [i.boot() for i in (self.fs, self.ps)]
-        
+
     def create_file(self,user,name,permission):
         return self.fs.create_file(user,name,permission)
-    
+
     def create_process(self,user,name):
         return self.ps.create_process(user,name)
+
+def main():
+    os = OperatingSystem()
+    os.start()
+    os.create_file('abc','witaj','-rw-r-r')
+    os.create_process('bar','ls/tmp')
+
+if __name__ == '__main__':
+    main()
