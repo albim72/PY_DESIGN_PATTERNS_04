9dd3079ce85a335544e2d22bc7630bece7826ecf
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 10:52:53 +0100

    Create concretebuilder1.py

---
+++ DESIGN_PATTERNS/concretebuilder1.py
@@ -0,0 +1,25 @@
+from prod1 import Product1
+from builder import Builder
+
+class ConcreteBuilder1(Builder):
+    
+    def __init__(self) -> None:
+        self.reset()
+    
+    def reset(self):
+        self._product = Product1()
+
+    @property
+    def product(self) -> Product1:
+        product = self._product
+        self.reset()
+        return product
+
+    def produce_part_a(self) -> None:
+        self._product.add("PartA1")
+
+    def produce_part_b(self) -> None:
+        self._product.add("PartB1")
+
+    def produce_part_c(self) -> None:
+        self._product.add("PartC1")
