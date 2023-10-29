efee88bfec601e1f6dae2bc53fb7ceeb5bed7c88
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 16:30:04 +0100

    Update fasadaOS.py

--- DESIGN_PATTERNS/fasadaOS.py
+++ DESIGN_PATTERNS/fasadaOS.py
@@ -16,16 +16,33 @@ class Server(metaclass=ABCMeta):
     @abstractmethod
     def __init__(self):
         pass
-    
+
     def __str__(self):
         return self.name
-    
+
     @abstractmethod
     def boot(self):
         pass
-    
+
     @abstractmethod
     def kill(self,restart=True):
         pass
-    
-    
+
+
+class FileServer(Server):
+    def __init__(self):
+        self.name = "FileServer"
+        self.state = State.new
+
+    def boot(self):
+        print(f'Bootowanie systemu {self}')
+        self.state = State.running
+
+    def kill(self, restart=True):
+        print(f'Ususwanie {self}')
+        self.state = State.restart if restart else State.zombie
+        
+    def create_file(self,user,name,permission):
+        print(f'próba utworzenia pliku: {name}, dla użytkownika: {user}, '
+              f'z uprawnieniami: {permission}')
+        
