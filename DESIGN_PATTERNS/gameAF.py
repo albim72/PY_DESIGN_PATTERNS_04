4f9ea2d08f1377f2b06800982da8fd7538d82f10
albim72 <marcin.albiniak72@gmail.com>
2023-10-29 14:37:47 +0100

    Update gameAF.py

--- DESIGN_PATTERNS/gameAF.py
+++ DESIGN_PATTERNS/gameAF.py
@@ -14,7 +14,7 @@ class Frog:
 
 class Bug:
     def __str__(self):
-        return 'robak'
+        return 'robaka'
 
     def action(self):
         return 'zjada go'
@@ -76,7 +76,32 @@ class GameEnvironment:
     def __init__(self,factory):
         self.hero = factory.make_character()
         self.obstacle = factory.make_obstacle()
-        
+
     def play(self):
         self.hero.interact_with(self.obstacle)
 
+#funkcja walidująca wiek
+def validate_age(name):
+    try:
+        age =None
+        age = int(input(f'Witaj {name}, podaj swój wiek w latach: '))
+        if not(0<=age<=120):
+            raise ValueError("")
+    except ValueError as er:
+        print(f'Wiek {age} nie jest prawidłowy, spróbuj ponownie wpisać...')
+        return (False,age)
+    return (True,age)
+
+
+def main():
+    name = input("Witaj, podaj imię swojego bohatera: ")
+    valid_input = False
+    while not valid_input:
+        valid_input,age = validate_age(name)
+    game = FrogWorld if age<15 else WarLockWorld
+    environment = GameEnvironment(game(name))
+    environment.play()
+
+
+if __name__ == '__main__':
+    main()
